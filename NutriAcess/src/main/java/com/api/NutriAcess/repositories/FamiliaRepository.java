744667a9f5996c6cf49afe84714da04553140eb7
Erica Inacia de Lima <98967783+EricaInaciadeLima@users.noreply.github.com>
2023-06-12 19:40:42 -0300

    Alterções

--- NutriAcess/src/main/java/com/api/NutriAcess/repositories/FamiliaRepository.java
+++ NutriAcess/src/main/java/com/api/NutriAcess/repositories/FamiliaRepository.java
@@ -3,9 +3,10 @@ package com.api.NutriAcess.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.api.NutriAcess.models.FamiliaModel;
-
+@Repository
 public interface FamiliaRepository extends JpaRepository<FamiliaModel, UUID>{
   
 }
